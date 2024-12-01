048899d8ef98e8b938124b1ac509202802964fce
Ashikur Rahman Hridoy <ashikur6053@gmail.com>
2024-12-01 21:15:20 +0600

    passing code

---
+++ Js/main.js
@@ -0,0 +1,8 @@
+const body = document.body;
+
+const h1 = document.createElement("h1");
+h1.innerText = "Ashikur Rahman";
+h1.setAttribute("class", "name");
+
+body.appendChild(h1);
+console.log(h1);
